9496ed8ef2a14807082783c153ecb5fecf92709d
nadianat <102003093+nadianat@users.noreply.github.com>
2022-04-29 15:50:15 +0800

    Add files via upload

---
+++ testhotel1/Data/testhotelWebContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using testhotel.Web.Models;
+
+namespace testhotel.Web.Data
+{
+    public class testhotelWebContext : DbContext
+    {
+        public testhotelWebContext (DbContextOptions<testhotelWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<testhotel.Web.Models.Reservation> Reservation { get; set; }
+    }
+}
---
+++ testhotel1/Models/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace testhotel.Web.Models
+{
+    public class Reservation
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Display(Name = "Check-In Date"), DataType(DataType.Date)]
+        public DateTime CheckInDate { get; set; }
+
+        [Display(Name = "Check-Out Date"), DataType(DataType.Date)]
+        public DateTime CheckOutDate { get; set; }
+        
+        [Required]
+        [Display(Name = "Room Type")]
+        public string? RoomType { get; set; }
+
+        [Display(Name = "Guest Amount")]
+        public int GuestAmount { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        //[NotMapped]
+        //public string CheckInDate => CheckInDate.ToString("dd/MM/yyyy");
+        //[NotMapped]
+        //public string CheckOutDate => CheckOutDate.ToString("dd/MM/yyyy");
+    }
+}

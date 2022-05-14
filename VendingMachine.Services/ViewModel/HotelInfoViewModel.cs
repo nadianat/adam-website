4fc3da50e86f00711515072c4a046ac781805794
nadianat <102003093+nadianat@users.noreply.github.com>
2022-05-14 10:50:52 +0800

    Add files via upload

---
+++ VendingMachine.Services/ViewModel/HotelInfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Services.ViewModel
+{
+    public class HotelInfoViewModel
+    {
+        public string HotelName { get; set; }
+        public string HotelID { get; set; }
+        public string HotelImageURL { get; set; }
+        public IEnumerable<RoomInfoViewModel> HotelRooms { get; set; }
+    }
+}
